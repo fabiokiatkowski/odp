df01dd430f14c3d08d200b1a2d19ea06adb99ef6
Lucas Kruger <lucas.kr1996@gmail.com>
2018-03-20 20:08:50 -0300

    Change tab-space to 2

--- src/Component/MachineOptionsSelect.js
+++ src/Component/MachineOptionsSelect.js
@@ -5,37 +5,36 @@ import Dialog, {
 } from 'material-ui/Dialog';
 
 import Paper from 'material-ui/Paper';
-import Menu from 'material-ui/Menu';
 
 export default class App extends Component {
     
-    render() {
-        const { selectMachine, options, isOpenMachineOptions } = this.props;
-        
-        return (<Dialog
-            title="Dialog With Actions"
-            open={isOpenMachineOptions}
-            style={{ maxWidth: 'none' }}
-            aria-labelledby="form-dialog-title"
-        >
-            <DialogTitle id="form-dialog-title">Machine options</DialogTitle>
-            <DialogContent style={{maxWidth:'none'}}>
-                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-arround' }}>
-                    {options.map((option, index) => (
-                        <Paper
-                            style={{
-                                padding: '20px',
-                                margin: '10px',
-                                cursor: 'pointer'
-                            }}
-                            key={index}
-                            onClick={() => selectMachine(option.id) }
-                        >
-                            { option.description }
-                        </Paper>
-                    ))}
-                </div>
-            </DialogContent>
-        </Dialog>);
-    }
+  render() {
+    const { selectMachine, options, isOpenMachineOptions } = this.props;
+    
+    return (<Dialog
+      title="Dialog With Actions"
+      open={isOpenMachineOptions}
+      style={{ maxWidth: 'none' }}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle id="form-dialog-title">Machine options</DialogTitle>
+        <DialogContent style={{maxWidth:'none'}}>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-arround' }}>
+            {options.map((option, index) => (
+              <Paper
+                style={{
+                  padding: '20px',
+                  margin: '10px',
+                  cursor: 'pointer'
+                }}
+                key={index}
+                onClick={() => selectMachine(option.id) }
+              >
+                { option.description }
+              </Paper>
+            ))}
+          </div>
+        </DialogContent>
+      </Dialog>);
+  }
 }
